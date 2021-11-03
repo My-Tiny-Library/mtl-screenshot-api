a04ab3665e2114e2ec9b2a9f89325e5311e41f66
Ilya Daraseliya <idarase@gmail.com>
2021-11-03 08:47:09 +0100

    fix

--- api/index.go
+++ api/index.go
@@ -15,15 +15,10 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func Screenshot(urlstr string) []byte {
-	opts := []chromedp.ExecAllocatorOption{
-		chromedp.ExecPath("/headless-shell/headless-shell"),
-	}
-	allocCtx, cancel := chromedp.NewExecAllocator(context.Background(), opts...)
-	defer cancel()
-
 	// create context
 	ctx, cancel := chromedp.NewContext(
-		allocCtx,
+		context.Background(),
+		// chromedp.WithDebugf(log.Printf),
 	)
 	defer cancel()
 
