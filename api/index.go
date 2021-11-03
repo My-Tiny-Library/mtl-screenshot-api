5e4b12a3e76c649cc6dabfe22898287e798bcf83
Ilya Daraseliya <idarase@gmail.com>
2021-11-03 08:23:44 +0100

    initial commit

---
+++ api/index.go
@@ -0,0 +1,49 @@
+package handler
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/chromedp/chromedp"
+)
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	var buf = string(Screenshot(r.URL.Query().Get("url")))
+	fmt.Fprintf(w, buf)
+}
+
+func Screenshot(urlstr string) []byte {
+	// create context
+	ctx, cancel := chromedp.NewContext(
+		context.Background(),
+		// chromedp.WithDebugf(log.Printf),
+	)
+	defer cancel()
+
+	// capture screenshot of an element
+	var buf []byte
+
+	// capture entire browser viewport, returning png with quality=90
+	if err := chromedp.Run(ctx, fullScreenshot(urlstr, 90, &buf)); err != nil {
+		log.Fatal(err)
+	}
+	// if err := ioutil.WriteFile("fullScreenshot.png", buf, 0o644); err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	log.Printf("wrote elementScreenshot.png and fullScreenshot.png")
+
+	return buf
+}
+
+// fullScreenshot takes a screenshot of the entire browser viewport.
+// Note: chromedp.FullScreenshot overrides the device's emulation settings. Reset
+func fullScreenshot(urlstr string, quality int, res *[]byte) chromedp.Tasks {
+	return chromedp.Tasks{
+		chromedp.Navigate(urlstr),
+		chromedp.EmulateViewport(1200, 630),
+		chromedp.FullScreenshot(res, quality),
+	}
+}
